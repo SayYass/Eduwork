248e56a600a189d0ccd4c607f028fafdc34e1874
SayYass <ilyasdewanto@gmail.com>
2021-12-23 13:59:39 +0700

    callback lesson

---
+++ Asynchronous - Callback/script.js
@@ -0,0 +1,27 @@
+function getData(url, success, error){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                success(xhr.response);
+            } else if (xhr.status === 404){
+                error();
+            }
+        }
+    }
+
+    xhr.open('get', url);
+    xhr.send();
+
+}
+
+getData('data/users.json' , results=>{
+const data = JSON.parse(results);
+tble.getElementById('data');
+data.forEach( m => console.log(m.name));
+
+} , 
+
+
+()=>{});
